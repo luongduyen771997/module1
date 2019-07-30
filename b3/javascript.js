ea9338c6655bd3e671c99d81a2dfaa9e424319c6
luongduyen7797 <duyenxinh7797@gmail.com>
2019-07-30 17:09:58 +0700

    add bài tập 3

---
+++ b3/javascript.js
@@ -0,0 +1,8 @@
+var a = prompt ("enter the width");
+var b = prompt("enter the height");
+let width = parseInt (a);
+let height = parseInt (b);
+let area = width * height
+document.write ("the area is:" + area);
+
+    
