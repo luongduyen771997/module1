864b2b97475b10cf23d4e74b74c8988cce095f98
luongduyen7797 <duyenxinh7797@gmail.com>
2019-07-31 17:11:44 +0700

    baitaptiente

---
+++ bài 4/myscript5.js
@@ -0,0 +1,27 @@
+const CONERT = 23000;
+function convertmoney(){
+   
+    let amounMoney =document.getElementById("AmountMoney").value;
+    let frommoney = document.getElementById("FromMoney").value;
+    let tomoney = document.getElementById("ToMoney").value;
+  
+    if(frommoney == "USD"){
+        if (tomoney == "USD"){
+            document.getElementById("result").innerHTML = "result: " + amounMoney + ' ' + tomoney;
+        }
+        else {
+            document.getElementById("result").innerHTML ="result:" + (amounMoney * 23000) + ' ' + tomoney;
+        }
+        }
+    else {
+            if(tomoney == "USD"){
+                document.getElementById("result").innerHTML = "result:" + (amounMoney /23000)+ ' ' + tomoney;
+            }
+            else{
+                document.getElementById("result").innerHTML ="result:" +' '+ tomoney;
+
+            }}
+        
+document.getElementById("result")= result ;
+
+}
