0b1e963f51026fc8a1445cd4a51455f37625efb3
luongduyen7797 <duyenxinh7797@gmail.com>
2019-08-13 22:53:49 +0700

    BT

---
+++ bài 4/quanly.js
@@ -0,0 +1,41 @@
+    
+let product = ["Sony","Samsung"];
+let data = '';
+let t =product.length;
+
+function Display() {	
+	for (var i = 0; i<t; i++) {
+        data += "<tr><td id='nameProduct'> " + product[i] + "</td><td><button class='btn btn-success' onclick='editProduct("+i+")'>Edit</button></td><td><button class='btn btn-success' onclick='delProduct("+i+")'>Delete</button></td></tr>";
+	}document.getElementById('table').innerHTML = data;	                                     
+    
+    }document.getElementById('table').innerHTML = data;	
+	data = '';
+
+
+function addto() {	
+    let pro=document.getElementById('product').value
+        product.push(pro);
+        console.log(product);
+	Display();
+} 
+
+function editProduct(abc) {
+    var name = prompt('Nhập tên sản phẩm','');
+	    for (var j = 0; j <t; j++) {		
+		if (product[abc] == product[j]) {
+			product[j] = name;
+			// console.log(product[abc]);
+		}
+	}
+	Display();
+}
+
+function delProduct(abc) {
+for (var j = 0; j <t; j++) {		
+		if (product[abc] == product[j]) {
+			product.splice(j,1);		
+			// console.log(product[abc]);
+		}
+	}
+	Display();
+}
